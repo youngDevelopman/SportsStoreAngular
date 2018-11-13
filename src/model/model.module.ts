03e4b2befe0ee97e813328fecdc27260a2129a65
young_developman <nazarmazurik99@gmail.com>
2018-11-13 21:26:35 +0200

    model building

---
+++ src/model/model.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { ProductRepository } from "./product.repository";
+import { StaticDataSource } from "./static.datasource";
+
+@NgModule({
+    providers:[ProductRepository,StaticDataSource]
+})
+
+export class ModelModule{
+
+}
