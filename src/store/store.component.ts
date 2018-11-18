c1bed6f2899a72248021bfed78a1e07e69c0a3b0
young_developman <nazarmazurik99@gmail.com>
2018-11-18 20:46:40 +0200

    use of filtering

--- src/store/store.component.ts
+++ src/store/store.component.ts
@@ -8,16 +8,20 @@ import { Component } from "@angular/core";
 })
 
 export class StoreComponent{
-
+    public selectedCategory = null;
     constructor(private repository:ProductRepository){
         
     }
     
     get products():Product[]{
-        return this.repository.getProducts();
+        return this.repository.getProducts(this.selectedCategory);
     }
 
     get categories():string[]{
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string){
+        this.selectedCategory = newCategory;
     }
 }
